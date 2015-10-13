8617759ab4a0966c7d71f5df2eac00c1288a8af8
Shani-08 <ShahjahanDurrani@hotmail.com>
2015-10-13 11:31:34 +0100

    bug fix return true

--- script.video.F4mProxy/lib/F4mProxy.py
+++ script.video.F4mProxy/lib/F4mProxy.py
@@ -351,46 +351,50 @@ class f4mProxy():
 class f4mProxyHelper():
 
     def playF4mLink(self,url,name,proxy=None,use_proxy_for_chunks=False, maxbitrate=0, simpleDownloader=False, auth=None, streamtype='HDS',setResolved=False,swf=None):
-        print "URL: " + url
-        stopPlaying=threading.Event()
-        progress = xbmcgui.DialogProgress()
+        try:
+            print "URL: " + url
+            stopPlaying=threading.Event()
+            progress = xbmcgui.DialogProgress()
 
-        
-        f4m_proxy=f4mProxy()
-        stopPlaying.clear()
-        runningthread=thread.start_new_thread(f4m_proxy.start,(stopPlaying,))
-        progress.create('Starting local proxy')
-        stream_delay = 1
-        progress.update( 20, "", 'Loading local proxy', "" )
-        xbmc.sleep(stream_delay*1000)
-        progress.update( 100, "", 'Loading local proxy', "" )
-        url_to_play=f4m_proxy.prepare_url(url,proxy,use_proxy_for_chunks,maxbitrate=maxbitrate,simpleDownloader=simpleDownloader,auth=auth, streamtype=streamtype, swf=swf)
-        listitem = xbmcgui.ListItem(name,path=url_to_play)
-        listitem.setInfo('video', {'Title': name})
+            
+            f4m_proxy=f4mProxy()
+            stopPlaying.clear()
+            runningthread=thread.start_new_thread(f4m_proxy.start,(stopPlaying,))
+            progress.create('Starting local proxy')
+            stream_delay = 1
+            progress.update( 20, "", 'Loading local proxy', "" )
+            xbmc.sleep(stream_delay*1000)
+            progress.update( 100, "", 'Loading local proxy', "" )
+            url_to_play=f4m_proxy.prepare_url(url,proxy,use_proxy_for_chunks,maxbitrate=maxbitrate,simpleDownloader=simpleDownloader,auth=auth, streamtype=streamtype, swf=swf)
+            listitem = xbmcgui.ListItem(name,path=url_to_play)
+            listitem.setInfo('video', {'Title': name})
 
-        if setResolved:
-            return url_to_play, listitem
-        mplayer = MyPlayer()    
-        mplayer.stopPlaying = stopPlaying
-        progress.close() 
-        mplayer.play(url_to_play,listitem)
-       
-        #xbmc.Player(xbmc.PLAYER_CORE_AUTO).play(url, listitem)
-        firstTime=True
-        while True:
-            if stopPlaying.isSet():
-                break;
-            #if not xbmc.Player().isPlaying():
-            #    break
-            xbmc.log('Sleeping...')
-            xbmc.sleep(200)
-            #if firstTime:
-            #    xbmc.executebuiltin('Dialog.Close(all,True)')
-            #    firstTime=False
-        stopPlaying.isSet()
+            if setResolved:
+                return url_to_play, listitem
+            mplayer = MyPlayer()    
+            mplayer.stopPlaying = stopPlaying
+            progress.close() 
+            mplayer.play(url_to_play,listitem)
+           
+            #xbmc.Player(xbmc.PLAYER_CORE_AUTO).play(url, listitem)
+            firstTime=True
+            played=False
+            while True:
+                if stopPlaying.isSet():
+                    break;
+                if xbmc.Player().isPlaying():
+                    played=True
+                xbmc.log('Sleeping...')
+                xbmc.sleep(200)
+                #if firstTime:
+                #    xbmc.executebuiltin('Dialog.Close(all,True)')
+                #    firstTime=False
+            stopPlaying.isSet()
 
-        print 'Job done'
-        return
+            print 'Job done'
+            return played
+        except: return False
+
         
     def start_proxy(self,url,name,proxy=None,use_proxy_for_chunks=False, maxbitrate=0,simpleDownloader=False,auth=None,streamtype='HDS',swf=None):
         print "URL: " + url
